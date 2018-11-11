bb3d6e9715bce3bd0c85873a1da68bbd801c19c8
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2018-11-11 09:14:53 +0700

    Hapus index.php di file config

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/admin';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
